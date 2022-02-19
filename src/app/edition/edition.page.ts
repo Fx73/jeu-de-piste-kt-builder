ad85b6f79492cd8047967ee0f89f2abb2d13e536
Fx73 <fx73000@yahoo.fr>
2022-02-19 21:26:49 +0100

    Display elements

--- src/app/edition/edition.page.ts
+++ src/app/edition/edition.page.ts
@@ -1,8 +1,10 @@
 import {CdkDragDrop, moveItemInArray, transferArrayItem} from '@angular/cdk/drag-drop';
 import { Component, OnInit } from '@angular/core';
+import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
 import { StageElement, TYPE } from './game/element/stage_element';
 
 import { ActivatedRoute } from '@angular/router';
+import { Element } from './game/element/element';
 import { Scenario } from './game/scenario';
 import { Stage } from './game/stage';
 
@@ -15,13 +17,14 @@ export class EditionPage implements OnInit {
   public static tools = [
     {name:'Texte', icon:'document-text', type : TYPE[TYPE.TXT]},
     {name:'Image', icon:'image' , type : TYPE[TYPE.IMG]},
-    {name:'Button', icon:'link' , type : TYPE[TYPE.BTN]},
+    {name:'Button', icon:'radio-button-on' , type : TYPE[TYPE.BTN]},
     {name:'Edit', icon: 'create' , type : TYPE[TYPE.EDT]},
     {name:'Step', icon: 'swap-horizontal' , type : TYPE[TYPE.ETP]},
     {name:'Toast', icon: 'sparkles' , type : TYPE[TYPE.TST]},
     {name:'QR', icon: 'qr-code' , type : TYPE[TYPE.QRC]},
     {name:'Lock', icon: 'lock-closed', type : TYPE[TYPE.LCK]},
     {name:'Unlock', icon: 'lock-open', type : TYPE[TYPE.UCK]},
+    {name:'Variable', icon: 'reorder-four', type : TYPE[TYPE.VAR]},
   ];
 
   public edition: string;
@@ -31,7 +34,10 @@ export class EditionPage implements OnInit {
    placeholdersize: number;
    placeholderindex = 0;
 
-  constructor(private activatedRoute: ActivatedRoute) {
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private sanitizer: DomSanitizer
+    ) {
   }
 
   ngOnInit() {
@@ -76,19 +82,46 @@ elementMove(event: CdkDragDrop<string[]>) {
 //#endregion
 
 //region UI
+getElementContent(element: StageElement): SafeHtml {
+  let html: string;
+  switch(element.type.toString()){
+    case 'TXT':
+      html = '<ion-input value="'+element.content+'"></ion-input>';
+      break;
+    case 'IMG':
+        html = '<ion-img src="'+this.getImage(element.content)+'"></ion-img>';
+      break;
+    case 'BTN':
+      html = '<ion-button color="medium" expand="block">'+element.content+'</ion-button>';
+          break;
+    case 'EDT':
+      html = '<ion-input placeholder="_____________________________________________________________" class="edt"></ion-input>'+
+      '<ion-button color="medium" expand="block">'+element.content+'</ion-button>';
+            break;
+    case 'ETP':
+      html = '<ion-input value="'+element.content+'"> -> Jump to step :</ion-input>';
+      break;
+    case 'TST':
+      html ='<ion-card></ion-item><ion-input value="'+element.content+'"></ion-input></ion-card>';
+              break;
+    case 'QRC':
+      html = '<ion-input value="'+element.content+'"></ion-input>';
+                break;
+    case 'LCK':
+      html = '<ion-input value="'+element.content+'"></ion-input>';
+                  break;
+     case 'UCK':
+      html = '<ion-input value="'+element.content+'"></ion-input>';
+                                break;
+  }
 
-addStage(){
-  this.scenario.stages.push(new Stage(''));
+return this.sanitizer.bypassSecurityTrustHtml(html);
 }
 
-
-
-getElementContent(element: StageElement){
-
-}
-
-getElementAdditional(element: StageElement){
-
+getElementAdditionalLabel(element: StageElement, index: number){
+  const getlabelfun = element.type.toString().toLowerCase() + 'AdditionalLabels';
+  const label: string = Element[getlabelfun]()[index];
+  return label;
 }
 
 getElementIcon(element: StageElement){
@@ -96,6 +129,9 @@ getElementIcon(element: StageElement){
   return tool?.icon ?? 'close-circle';
 }
 
+addStage(){
+  this.scenario.stages.push(new Stage(''));
+}
 //#endregion
 
 
@@ -108,6 +144,9 @@ getTools(){
     this.scenario = JSON.parse(json);
   }
 
+  getImage(url: string){
+    return 'assets/icon/favicon.png';
+  }
 
 //#endregion
 
--- src/app/edition/game/element/element.ts
+++ src/app/edition/game/element/element.ts
@@ -8,4 +8,103 @@
      public content: string,
      public additional: Array<string> = Array()
    ){}
+
+
+ /**
+  * TXT: Instantiate a qr code waiter
+  *
+  * @param content : text text
+  */
+  public static txtAdditionalLabels(): string[]{
+    return [];
+ }
+
+
+/**
+ * IMG: Instantiate an image
+ *
+ * @param content : image name
+ */
+ public static imgAdditionalLabels(): string[]{
+  return [];
+}
+
+ /**
+  * BTN: Instantiate a button
+  *
+  * @param content : text of button (will be the name of the listener)
+  * @param additional = 1 - name of under stage to run
+  */
+  public static btnAdditionalLabels(): string[]{
+    return ['name of under stage to run'];
+ }
+
+/**
+ * EDT: Instantiate a edit text
+ *
+ * @param content : text to show
+ *  @param Additional (3) = 1- name of under stage to run / 2 -  answer possible / 3 - answer possible
+ */
+
+ public static edtAdditionalLabels(): string[]{
+  return ['name of under stage to run',
+'possible answer',
+'possible answer'];
+}
+
+/**
+ * ETP: Instantiate an Phase
+ *
+ * @param content : step id
+ */
+ public static etpAdditionalLabels(): string[]{
+  return [];
+}
+
+
+/**
+ * QRC: Instantiate a qr code waiter
+ *
+ * @param content : qr content = name of the under stage to run
+ */
+ public static qrcAdditionalLabels(): string[]{
+  return [];
+}
+
+/**
+ * TST: Instantiate a toast
+ *
+ * @param content : toast text
+ */
+ public static tstAdditionalLabels(): string[]{
+  return [];
+}
+
+/**
+ * LCK: Instantiate a lock
+ *
+ * @param content : lock id
+ */
+ public static lckAdditionalLabels(): string[]{
+  return [];
+}
+
+/**
+ * UCK: Instantiate an unlock
+ *
+ * @param content : lock name
+ *
+ */
+ public static uckAdditionalLabels(): string[]{
+  return [];
+}
+
+/**
+ * VAR: Instantiate a variables
+ *
+ * @param content : variables content : "var=value"
+ */
+ public static varAdditionalLabels(): string[]{
+  return [];
+}
  }
