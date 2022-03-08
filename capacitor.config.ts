0628cdffc701c27ffb731ee7a9431415d620a590
Fx73 <fx73000@yahoo.fr>
2022-03-08 02:08:07 +0100

    firebase config

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
   appName: 'Jeu-de-Piste-Kt-Builder',
-  webDir: 'www',
+  webDir: 'website/www',
   bundledWebRuntime: false
 };
 
